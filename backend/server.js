f74f53a3325793ad939c5a4e7c2dbf38b7c13e27
Prajjawal <prajjawaldy.zhcet@gmail.com>
2024-11-04 20:53:12 +0530

    eddited db.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://simple-auth-wivv-frontend-qgo9mov7v-prajjawal12s-projects.vercel.app/',
+    ],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use('/api', authRoutes);
