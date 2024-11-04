39931e74b2dea4a005db434222bfd56c9a70cc76
Prajjawal <prajjawaldy.zhcet@gmail.com>
2024-11-04 23:53:57 +0530

    edited endpints url

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,21 @@ const app = express();
 app.use(
   cors({
     origin: 'https://simple-auth-wivv-frontend.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   })
 );
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigins.join(','));
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE, OPTIONS');
+  next();
+});
 
 app.options('*', cors());
 app.use('/api', authRoutes);
