d40d0c35ff61aa4f04a7f9beddeeed011c28679f
Prajjawal <prajjawaldy.zhcet@gmail.com>
2024-11-04 21:08:33 +0530

    editedaas

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: ['https://simple-auth-wivv-frontend.vercel.app/'],
+    origin: ['https://simple-auth-wivv-frontend.vercel.app'],
     methods: ['POST', 'GET'],
     credentials: true,
   })
